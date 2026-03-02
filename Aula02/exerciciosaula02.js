86038e6d62e266f95ac6f0aab0ab5f644e90d5a1
Laura_Reis <150523998+L4ur4r3eiz@users.noreply.github.com>
2026-03-02 08:09:14 -0300

    Add files via upload

---
+++ Aula02/exerciciosaula02.js
@@ -0,0 +1,57 @@
+//Questão A da atividade 1
+var a = "10";
+var b = "20";
+
+console.log(b)
+b = 5; 
+
+console.log(a, b); 
+
+//Questão B da atividade 1
+
+a = 30;
+b = 20;
+
+c = a + b;
+
+console.log(c)
+
+b = 10;
+
+console.log(b, c)
+console.log(a, b, c)
+
+//Questão C da atividade 1
+
+var a = "10";
+var b = "20";
+
+var c = a;
+var b = c;
+
+var a = b;
+
+console.log(a, b, c)
+
+
+//Questão D da atividade 1
+
+a = 10;
+b = a + 1;
+a = b + 1;
+
+b = a + 1;
+
+console.log(a)
+a = b + 1;
+console.log(a, b)
+
+
+
+
+
+
+
+
+
+
